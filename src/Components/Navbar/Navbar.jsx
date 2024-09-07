0b3acd8e9f08611872a7f5f02ab94dec3bdce746
chandrakantBirajdar <chandrakantbirajdar986@gmail.com>
2024-09-07 21:54:18 +0530

    Navbar is completed

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import './Navbar.css'
+function Navbar(){
+
+
+    return(
+
+        <div className='main-container'>
+            <nav>
+                <div>
+                    <img src="" alt="" />
+                    <h3>Education</h3>
+                </div>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Login</li>
+                </ul>
+
+            </nav>
+        </div>
+    );
+}
+export default Navbar;
